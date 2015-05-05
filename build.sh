6b4c6b63357604b9a4071656ce5f97e9baf32028
Eric Hanson <eric@aquameta.com>
2015-05-04 18:15:40 -0700

    merge, broken state

--- build.sh
+++ build.sh
@@ -15,4 +15,4 @@ echo "select bundle.checkout(c.id) from bundle.commit c join bundle.bundle b on 
 echo "Loading semantics ..."
 cat core/0*/semantics.sql  | psql -a aquameta 2>&1 | grep -B 2 -A 10 ERROR:
 
-
+# echo "insert into remote_http (bundle_id, endpoint_url) values ('737177af-16f4-40e1-ac0d-2c11b2b727e9'::uuid, 'http://demo.aquameta.org/endpoint');" | psql aquameta
--- core/004-www/001-www-client.sql
+++ core/004-www/001-www-client.sql
@@ -91,10 +91,52 @@ response = urllib2.urlopen(req)
 raw_response = response.read()
 return raw_response
 
+
+create or replace function www_client.http_post(url text, my_args json)
+returns text
+as $$
+import urllib
+import urllib2
+
+import json
+import plpy
+
+json_args = json.loads(my_args)
+# plpy.log(json_args)
+#url = url
+#
+r = requests.post(url) #, data=_args)
+
+return r.status_code
+ 
 $$ language plpythonu;
+
+
+create or replace function www_client.http_post (url text, bundle_id uuid) returns text
+as $$
+
+import urllib2
+import urllib
+import plpy
+
+
+# get from repo
+# which commits do i need not include?
+
+url = 'http://bazaar.aquameta.com'
+
+stmt = plpy.prepare('select * from bundle.commit where bundle_id = $1', [ 'uuid' ]);
+commits = plpy.execute(stmt, [ bundle_id ]);
+
+data = urllib.urlencode(commits)
+req = urllib2.Request(url, data)
+response = urllib2.urlopen(req)
+raw_response = response.read()
+return raw_response
+
+$$ language plpythonu;
+
 */
-
-
 
 
 
@@ -124,34 +166,28 @@ select www_client.http_get ((select url from www.remote_http where id=http_remot
 $$ language sql;
 
 
+/*******************************************************************************
+* rows_insert
+*******************************************************************************/
+/*
+create or replace function www_client.rows_insert(http_remote_id uuid, args json[], out response json)
+as $$
+
+select www_client.http_post (
+    (
+        select endpoint_url from bundle.remote_http where id=http_remote_id)
+            || '/insert',
+        (array_to_json(args))::text
+
+    )::json;
+
+$$ language sql;
+*/
+
 
 /*******************************************************************************
 * row_select
 *******************************************************************************/
-create or replace function www_client.http_post (url text, bundle_id uuid) returns text
-as $$
-
-import urllib2
-import urllib
-import plpy
-
-
-# get from repo
-# which commits do i need not include?
-
-url = 'http://bazaar.aquameta.com'
-
-stmt = plpy.prepare('select * from bundle.commit where bundle_id = $1', [ 'uuid' ]);
-commits = plpy.execute(stmt, [ bundle_id ]);
-
-data = urllib.urlencode(commits)
-req = urllib2.Request(url, data)
-response = urllib2.urlopen(req)
-raw_response = response.read()
-return raw_response
-
-$$ language plpythonu;
-
 
 
 --
@@ -170,7 +206,90 @@ $$ language plpythonu;
 --
 --
 --
---
+
+
+
+
+/*******************************************************************************
+*
+*
+* BUNDLE CONNECTIONS
+*
+*
+*******************************************************************************/
+
+
+
+create or replace function bundle.compare(in remote_http_id uuid)
+returns table(local_commit_id uuid, remote_commit_id uuid)
+as $$
+declare
+begin
+    return query
+        with remote_commit as (select (json_array_elements(
+                www_client.http_get(
+                    r.endpoint_url
+                        || '/bundle/table/commit/rows?bundle_id='
+                        || r.bundle_id
+                )::json->'result')->'row'->>'id')::uuid as id
+            from bundle.remote_http r
+            where r.id = remote_http_id
+        )
+        select c.id as local_commit_id, rc.id as remote_id
+        from bundle.commit c
+            full outer join remote_commit rc on rc.id=c.id;
+
+end;
+$$ language  plpgsql;
+
+
+create or replace function bundle.push(in remote_http_id uuid)
+returns void -- table(_row_id meta.row_id)
+as $$
+declare
+    records json[];
+begin
+    -- commits to push
+    create temporary table _bundlepacker_tmp (row_id meta.row_id, next_fk uuid);
+
+    -- bundle
+    insert into _bundlepacker_tmp
+        select meta.row_id('bundle','bundle','id', bundle.id::text), null
+        from bundle.bundle 
+        join bundle.remote_http on remote_http.bundle_id=bundle.id;
+
+    -- commit
+    with unpushed_commits as (
+        select commit.id from bundle.compare(remote_http_id) comp
+            join bundle.commit on commit.id = comp.local_commit_id
+            -- join bundle.remote_http r on r.bundle_id = c.id FIXME?
+        -- where r.id = remote_http_id
+            and comp.remote_commit_id is null)
+     insert into _bundlepacker_tmp select meta.row_id('bundle','commit','id', id::text), rowset_id from bundle.commit
+        where commit.bundle_id::text in (select (row_id).pk_value from _bundlepacker_tmp)
+            and commit.id in (select id from unpushed_commits);
+
+    -- rowset
+    insert into _bundlepacker_tmp select meta.row_id('bundle','rowset','id', id::text), null from bundle.rowset
+        where id in (select next_fk from _bundlepacker_tmp where (row_id::meta.relation_id).name = 'commit');
+
+    -- rowset_row
+    insert into _bundlepacker_tmp select meta.row_id('bundle','rowset_row','id', id::text), rowset_id from bundle.rowset_row rr
+        where rr.rowset_id::text in (select (row_id).pk_value from _bundlepacker_tmp where (row_id::meta.relation_id).name = 'rowset');
+
+    -- rowset_row_field
+    insert into _bundlepacker_tmp select meta.row_id('bundle','rowset_row_field','id', id::text), rowset_row_id from bundle.rowset_row_field rr
+        where rr.rowset_row_id::text in (select (row_id).pk_value from _bundlepacker_tmp where (row_id::meta.relation_id).name = 'rowset_row');
+
+    -- perform array_append(records, to_json(r)) from _bundlepacker_tmp r;
+
+    perform www_client.rows_insert(remote_http_id, bp) from _bundlepacker_tmp bp;
+
+    -- RETURN QUERY EXECUTE  'select row_id, meta.row_id_to_json(row_id) from _bundlepacker_tmp';
+    -- RETURN QUERY EXECUTE 'select * from _bundlepacker_tmp';
+
+end;
+$$ language plpgsql;
 
 
 commit;
